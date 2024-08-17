b075f7847acb9392fef340a8a785760376f2c576
Connor Goldberg <connor@connorgoldberg.com>
2024-08-17 15:46:28 -0400

    Clean up settings dialog

--- src/components/settings-dialog.jsx
+++ src/components/settings-dialog.jsx
@@ -9,15 +9,22 @@ import { Button } from "@/components/ui/button"
 import { Switch } from "@/components/ui/switch"
 import { ScrollArea } from "@/components/ui/scroll-area"
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, DrawerHeader, DrawerTitle, DrawerTrigger } from "@/components/ui/drawer"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
+import { Card, CardContent } from "@/components/ui/card"
+import { Separator } from "@/components/ui/separator"
 import { Settings } from "lucide-react"
 import { SettingsSchema } from "@/components/settings"
+import { DefaultSettings } from "@/components/settings"
 
 // import { ServerFormHandler } from "./server-form-handler"
 
 export function SettingsDialog({onSubmit, options}) {
-  const [open, setOpen] = React.useState(false)
+  const [settingsOpen, setSettingsOpen] = React.useState(false)
+
+  // Need to maintain the resetDialog state here so it doesn't get removed on re-render when the breakpoint changes
+  const [resetOpen, setResetOpen] = React.useState(false)
 
   // Equal to Tailwind's "md" breakpoint
   const isDesktop = useMediaQuery("(min-width: 768px)")
@@ -25,9 +32,18 @@ export function SettingsDialog({onSubmit, options}) {
   const title = "Settings"
   const desc  = `Make changes to the settings here. ${isDesktop ? "Click" : "Tap"} save when you're done.`
 
+  const settingsFormProps = {
+    onSubmit: onSubmit,
+    setSettingsOpen: setSettingsOpen,
+    isDesktop: isDesktop,
+    options: options,
+    resetOpen: resetOpen,
+    setResetOpen: setResetOpen,
+  }
+
   if (isDesktop) {
     return (
-      <Dialog open={open} onOpenChange={setOpen}>
+      <Dialog open={settingsOpen} onOpenChange={setSettingsOpen}>
         <DialogTrigger asChild>
           <Button variant="outline" size="icon">
             <Settings className="w-3/5 h-3/5"/>
@@ -38,8 +54,8 @@ export function SettingsDialog({onSubmit, options}) {
             <DialogTitle>{title}</DialogTitle>
             <DialogDescription>{desc}</DialogDescription>
           </DialogHeader>
-          <ScrollArea className="overflow-y-auto">
-            <ProfileForm onSubmit={onSubmit} setOpen={setOpen} options={options} />
+          <ScrollArea className="overflow-y-auto" scrollHideDelay="0">
+            <SettingsForm {...settingsFormProps} />
           </ScrollArea>
         </DialogContent>
       </Dialog>
@@ -47,7 +63,7 @@ export function SettingsDialog({onSubmit, options}) {
   }
 
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
+    <Drawer open={settingsOpen} onOpenChange={setSettingsOpen}>
       <DrawerTrigger asChild>
         <Button variant="outline" size="icon">
           <Settings className="w-3/5 h-3/5" />
@@ -59,7 +75,7 @@ export function SettingsDialog({onSubmit, options}) {
           <DrawerDescription>{desc}</DrawerDescription>
         </DrawerHeader>
         <ScrollArea className="overflow-y-auto">
-          <ProfileForm onSubmit={onSubmit} setOpen={setOpen} options={options} className="px-4" />
+          <SettingsForm {...settingsFormProps} className="px-4" />
           <DrawerFooter className="pt-2">
             {/* <DrawerClose asChild>
               <Button variant="outline">Cancel</Button>
@@ -71,7 +87,51 @@ export function SettingsDialog({onSubmit, options}) {
   )
 }
 
-function ProfileForm({ className, onSubmit, setOpen, options }) {
+function FormSwitch({form, name, label}) {
+  return <FormField
+    control={form.control}
+    name={name}
+    render={({ field }) => (
+      <FormItem>
+        <div className="flex items-center gap-4">
+          <FormLabel className="min-w-40">{label}</FormLabel>
+          <div className="grow"/>
+          <FormControl>
+            <Switch checked={field.value} onCheckedChange={field.onChange} />
+          </FormControl>
+        </div>
+        <FormMessage />
+      </FormItem>
+    )}
+  />
+}
+
+function ResetButton(props) {
+
+  return (
+    <Dialog {...props}>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="w-full md:w-auto md:min-w-[8em]">Reset to Defaults</Button>
+      </DialogTrigger>
+      <DialogContent className="w-[90%] overflow-hidden flex flex-col rounded-lg">
+        <DialogHeader className="text-left sm:text-left">
+          <DialogTitle>Confirm Changes</DialogTitle>
+          <DialogDescription>Are you sure you want to reset settings to their default values?</DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col md:flex-row gap-2">
+          <DialogClose asChild>
+            <Button onClick={props.onReset} variant="destructive" className="w-full md:w-auto md:min-w-[8em]">Yes</Button>
+          </DialogClose>
+          <DialogClose asChild>
+            <Button variant="outline" className="w-full md:w-auto md:min-w-[8em]">Cancel</Button>
+          </DialogClose>
+        </div>
+      </DialogContent>
+    </Dialog>
+  )
+}
+
+function SettingsForm({ className, ...props }) {
 
   const [pending, setPending] = React.useState(false)
 
@@ -80,55 +140,50 @@ function ProfileForm({ className, onSubmit, setOpen, options }) {
     // const result = await ServerFormHandler(formData)
     // setPending(false)
     // console.log(result)
-    onSubmit(formData)
-    setOpen(false)
+    props.onSubmit(formData)
+    props.setSettingsOpen(false)
   }
 
   const form = useForm({
     resolver: zodResolver(SettingsSchema),
-    defaultValues: options
+    defaultValues: props.options
   })
+
+  const switches = [
+    {
+      name: "johnMode",
+      label: "John Mode"
+    },
+    {
+      name: "showAnimation",
+      label: "Show Animation"
+    }
+  ]
 
   return (
     <Form {...form}>
       <form action={form.handleSubmit(onSubmitWrapper)} className={cn("space-y-4 py-1", className)}>
+        <FormLabel className="text-lg">Switches</FormLabel>
+        <Card>
+          <CardContent className="p-2">
+            <div className="space-y-2">
+              {switches.map((item, index) => (
+                <>
+                  <FormSwitch form={form} name={item.name} label={item.label}/>
+                  {index < switches.length-1 && <Separator/>}
+                </>
+              ))}
+            </div>
+          </CardContent>
+        </Card>
 
-        <FormField
-          control={form.control}
-          name="johnMode"
-          render={({ field }) => (
-            <FormItem>
-              <div className="flex items-center gap-4">
-                <FormLabel className="min-w-40">John Mode</FormLabel>
-                <FormControl>
-                  <Switch checked={field.value} onCheckedChange={field.onChange} />
-                </FormControl>
-              </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="showAnimation"
-          render={({ field }) => (
-            <FormItem>
-              <div className="flex items-center gap-4">
-                <FormLabel className="min-w-40">Show Animation</FormLabel>
-                <FormControl>
-                  <Switch checked={field.value} onCheckedChange={field.onChange} />
-                </FormControl>
-              </div>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <Button type="submit" disabled={pending} variant="outline" className="w-full md:w-auto md:min-w-[8em]">
-          {!pending && "Save"}
-          { pending && "Saving..."}
-        </Button>
+        <div className="flex flex-col gap-y-2 md:flex-row">
+          <Button type="submit" disabled={pending} className="w-full md:w-auto md:min-w-[8em]">
+            {pending ? "Saving" : "Save"}
+          </Button>
+          {props.isDesktop && <div className="grow"/>}
+          <ResetButton onReset={() => onSubmitWrapper(DefaultSettings)} open={props.resetOpen} onOpenChange={props.setResetOpen} />
+        </div>
       </form>
     </Form>
   )
