26d9743ba6ef9c19f5e5bc08e8c14d486831aaaa
Connor Goldberg <connor@connorgoldberg.com>
2024-09-04 00:48:37 -0400

    Add icons

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,6 +23,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon?<generated>" type="image/<generated>" sizes="<generated>" />
+        <link rel="apple-touch-icon" href="/apple-icon?<generated>" type="image/<generated>" sizes="<generated>" />
+      </head>
       <body className={cn("bg-background font-sans antialiased", fontSans.variable, fontWarzone.variable)}>
         <ThemeProvider
           attribute="class"
